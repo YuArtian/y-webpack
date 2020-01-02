fdfc62c4f065182cbecc6aaad7ce3b1a0aa4f9df
XueYu.Zhao <xueyu.zhao@oriente.com>
2020-01-02 18:42:05 +0800

    update ReadMe && add bin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # y-webpack
 自己简单实现的webpack
+
+基本流程
+1. 执行 bin/y-webpack.js<br>
+   1.1 根据命令行或配置文件生成配置<br>
+   1.2 根据配置进行 y-webpack 打包逻辑
+2. AST语法转换<br>
+   2.1 [babel/parser](https://babeljs.io/docs/en/babel-parser) 解析语法 生成AST<br>
+   2.2 [babel/traverse](https://babeljs.io/docs/en/babel-traverse) 遍历AST 分析记录依赖关系<br>
+   2.3 [babel/core](https://babeljs.io/docs/en/babel-core) babel.transformFromAst + [babel/preset-env](https://babeljs.io/docs/en/babel-preset-env) 将AST转换为ES5 JavaScript
+3. 处理模块加在依赖
+4. 生成一个可以在浏览器加载执行的 js 文件
---
+++ bin/y-webpack.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const argv = require('yargs').argv
+const ora = require('ora')
+const chalk = require('chalk')
+const y_webpack = require('../lib/y-webpack')
+//当前工作目录
+const projectPath = process.cwd()
+//当前配置文件
+const configPath = path.join(projectPath, 'y-webpack.config.js')
+
+/* 内置默认配置 */
+let _config = Object.create(null)
+//入口
+_config.entry = 'src/index.js'
+//出口
+_config.output = 'dist/bunlde.js'
+
+/* 配置文件处理 */
+if(fs.existsSync(configPath)){
+  let config = require(configPath)
+  _config = Object.assign(Object.create(null), _config, config)
+}
+
+/* 命令行参数处理 */
+let options = argv._
+//第一个参数为入口配置
+if (options[0]) {
+  _config.entry = options[0]
+}
+//第二个参数为出口配置
+if (options[1]) {
+  _config.output = options[1]
+}
+
+/* 最终可用配置处理 */
+//实际入口路径
+_config.entry = path.join(projectPath, _config.entry)
+_config.output = path.join(projectPath, _config.output)
+
+/* 初始化 */
+function init () {
+  let spinner = ora('开始打包')
+  spinner.start()
+  //找不到入口文件
+  if (!fs.existsSync(_config.entry)) {
+    spinner.stop()
+    chalk.red('找不到入口文件')
+  }
+  //获取打包结果
+  const result = y_webpack(_config)
+  //输出打包结果
+  try {
+    fs.mkdirSync(path.dirname(_config.output))
+    fs.writeFileSync(_config.output, result)
+  } catch (error) {
+    spinner.stop()
+    chalk.red('打包输出出错')
+  }
+  spinner.stop()
+  chalk.green('已生成对应文件')
+}
+
+init()
